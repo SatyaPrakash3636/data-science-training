d432ad66b8c43978b7105b3c3e03aefbf5d76d45
satyaprakash3636 <satyaprakash3636@gmail.com>
2022-04-19 00:09:58 +0530

    Arrange input string in the ascending order

---
+++ day-1/exercise_solution/sol8.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+# Arrange the characters of input string in the ascending order
+
+# Take user input
+user_input = input("Enter the string: ")
+
+# Sort the input
+sorted_data = sorted(user_input)
+
+# Printing sorted data
+print(f"Sorted string: {sorted_data}")
