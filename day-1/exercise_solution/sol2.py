a221c78edd12e437427fc507861d1381127103eb
satyaprakash3636 <satyaprakash3636@gmail.com>
2022-04-18 19:36:59 +0530

    Printing last 3 characters of input string

---
+++ day-1/exercise_solution/sol2.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+# Printing last 3 characters of input string
+
+# Asking user for input
+user_input = input("Enter input string: ")
+
+# Get last 3 characters
+sliced_data = user_input[-3:]
+
+# Printing to console
+print(f"Last 3 characters of input data: {sliced_data}")
