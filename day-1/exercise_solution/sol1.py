c48e8d81f5fd0fd1db2bd139a3e1ac3bd2268a72
Satya Prakash <satyaprakash3636@gmail.com>
2022-04-18 19:30:23 +0530

    Reversing input string

--- day-1/exercise_solution/sol1.py
+++ day-1/exercise_solution/sol1.py
@@ -1,18 +1,14 @@
 #! /usr/bin/python3
 
-#
 # Reversing input string
-#
 
 # Asking user for input 
-
 input_string = input("Enter the string: ")
 
 # Reversing input string
 reversed_string = input_string[::-1]
 
 # Reversing input string with reversed method
-
 reversed_string1 = "".join(list(reversed(input_string)))
 
 # Printing the reversed string
