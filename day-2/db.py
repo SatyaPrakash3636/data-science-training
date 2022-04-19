1b1c139ec9485afd0f2ff7c59909d871bbcc0003
Satya Prakash <satyaprakash3636@gmail.com>
2022-04-19 15:42:52 +0530

    Connect to MySQL database

--- day-2/db.py
+++ day-2/db.py
@@ -1,16 +1,17 @@
 import pymysql
 
 conn = pymysql.connect(
-    host="192.168.225.201", user="satya", password="satya", database="python"
+    host="192.168.225.201", user="satya", password="satya", database="world"
 )
 
 cursor = conn.cursor()
 
 stmt1 = "show tables"
-stmt2 = "select * from scripts"
+stmt2 = "select * from city"
 
 cursor.execute(stmt2)
 
 data1 = cursor.fetchall()
 
-print(data1)
+for data in data1:
+    print(data)
